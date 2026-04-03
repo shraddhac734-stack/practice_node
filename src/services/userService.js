82edb4a4ee14a072736f75ecb2c1a38ba93b69ed
shraddha <shraddhac734@gmail.com>
2026-04-03 11:08:03 +0530

    add validation

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,16 @@
 const UserRepository = require("../repositories/userRepo");
+const bcrypt = require("bcrypt")
+const Response = require("../utils/response")
 class userService {
   // Create User
   async addUser(data) {
+    const { email, name, password } = data;
+    if (!name || !email || !password) {
+      throw new Error("All fields are required");
+    }
+    //password becrypt
+    data.password=await bcrypt.hash(password,10);
+
     // store data into and return it
     return await UserRepository.addUser(data);
   }
